1542dc95399945f3674053b9f6d633f53f97948c
JoaoPedroVisconti <visconti.joao@gmail.com>
2022-05-13 16:56:32 +0100

    Include the config fili on Git

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://visconti:xU5!vr4qp562Yiu@cluster0.xbtfw.mongodb.net/BugTracker?retryWrites=true&w=majority',
+
+  SECRET_KEY: 'some very secret key',
+}
